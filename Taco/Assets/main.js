6332744f86274a05cb5ec655de3ed9c942a117e0
jugagd <43351264+jugagd@users.noreply.github.com>
2018-10-11 13:28:15 -0300

    'Taco'

---
+++ Taco/Assets/main.js
@@ -0,0 +1,15 @@
+//Menu
+function openMenu(evt, menuName) {
+  var i, x, tablinks;
+  x = document.getElementsByClassName("menu");
+  for (i = 0; i < x.length; i++) {
+     x[i].style.display = "none";
+  }
+  tablinks = document.getElementsByClassName("tablink");
+  for (i = 0; i < x.length; i++) {
+     tablinks[i].className = tablinks[i].className.replace(" w3-green", "");
+  }
+  document.getElementById(menuName).style.display = "block";
+  evt.currentTarget.firstElementChild.className += " w3-green";
+}
+document.getElementById("myLink").click();
