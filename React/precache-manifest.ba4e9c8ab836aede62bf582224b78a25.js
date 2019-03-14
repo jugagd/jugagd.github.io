287474072418dc923f274fa7bf53c45f0389ec49
jugagd <43351264+jugagd@users.noreply.github.com>
2019-03-14 12:38:42 -0300

    'react'

---
+++ React/precache-manifest.ba4e9c8ab836aede62bf582224b78a25.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "6c19c111359faf52ba4a",
+    "url": "/static/js/main.d011f2ab.chunk.js"
+  },
+  {
+    "revision": "ad9a9452a284a9d76bbd",
+    "url": "/static/js/2.8661b869.chunk.js"
+  },
+  {
+    "revision": "6c19c111359faf52ba4a",
+    "url": "/static/css/main.b3214077.chunk.css"
+  },
+  {
+    "revision": "506efb1634387a96fe82aa16c145f4cc",
+    "url": "/index.html"
+  }
+];
