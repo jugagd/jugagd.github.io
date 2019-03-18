bb36902bd750e0cf4dbec6ccdd0b934436f89a74
jugagd <43351264+jugagd@users.noreply.github.com>
2019-03-18 11:51:10 -0300

    'cartapp'

---
+++ CartApp/precache-manifest.21b6698309ad700c883e0bca1b945958.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/CartApp/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "69161627ef021d9df46b",
+    "url": "/CartApp/static/js/runtime~main.8da04f55.js"
+  },
+  {
+    "revision": "f13099de19e24c876589",
+    "url": "/CartApp/static/js/main.7e149462.chunk.js"
+  },
+  {
+    "revision": "6080e74a6bc3b8d48b49",
+    "url": "/CartApp/static/js/2.8500544b.chunk.js"
+  },
+  {
+    "revision": "f13099de19e24c876589",
+    "url": "/CartApp/static/css/main.27cf9a88.chunk.css"
+  },
+  {
+    "revision": "fc75838733bd3dc155bd76b207f4f938",
+    "url": "/CartApp/index.html"
+  }
+];
